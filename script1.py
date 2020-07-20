138b6469ac95103d4c077d733d8da53bbdffde31
pooja srinivasan <58687165+poojaseenu1999@users.noreply.github.com>
2020-07-20 14:32:41 +0530

    deleting,selecting,updating

--- script1.py
+++ script1.py
@@ -13,7 +13,7 @@ def insert(item,quantity,prize):
     cur.execute("INSERT INTO store VALUES(?,?,?)",(item,quantity,prize))
     conn.commit()
     conn.close()
-insert("cofee cups",4,40.50)
+#insert("cofee cups",4,40.50)
 
 def view():
     conn=sqlite3.connect("lite.db")
@@ -23,4 +23,22 @@ def view():
     conn.close()
     return rows
 
+
+def delete(item):
+    conn=sqlite3.connect("lite.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM store WHERE item=?",(item,))
+    conn.commit()
+    conn.close()
+
+
+def update(quantity,prize,item):
+    conn=sqlite3.connect("lite.db")
+    cur=conn.cursor()
+    cur.execute("UPDATE store SET quantity=?,prize=? WHERE item=?",(quantity,prize,item))
+    conn.commit()
+    conn.close()
+
+update(10,70.90,"water bottle")
+#delete("cofee cups")
 print(view())
